2dbb3765c08f23757d965eef2e108102e101d15d
shaimesisterano <shai@mcterano.com>
2018-12-25 10:24:51 +0200

    express parameters

--- express-rest-api/index.js
+++ express-rest-api/index.js
@@ -1,14 +1,34 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var app = express();
 
-app.get('/users', function(request, response){
+// app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+
+// app.get('/users', function(request, response){
+//     console.log(`GET users result`);
+//     console.log(`COUNTRY: ${request.query.country}`);
+//     console.log(`ID: ${request.query.id}`);
+//     response.end();
+// });
+
+app.get('/users/:userId/articles/:articleId', function(request, response){
     console.log(`GET users result`);
+    console.log(`COUNTRY: ${request.params.userId}`);
+    console.log(`ID: ${request.params.articleId}`);
     response.end();
 });
 
+// app.post('/users', function(request, response){
+//     console.log(`POST users result`);
+//     response.end();
+// });
+
 app.post('/users', function(request, response){
     console.log(`POST users result`);
+    console.log(`USER_ID: ${request.body.userId}`);
+    console.log(`ARTICLE_ID: ${request.body.articleId}`);
     response.end();
 });
 
