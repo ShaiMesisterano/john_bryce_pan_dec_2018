5335db9ac38ccd7bd369cbaf21ce35e5ed7fe478
shaimesisterano <shai@mcterano.com>
2018-12-31 00:52:28 +0200

    exercise 9 solution

---
+++ exercises/9/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var rp = require('request-promise');
+var jsdom = require('jsdom');
+var { JSDOM } = jsdom;
+
+var app = express();
+
+app.get('/', async function(request, response){
+    var search = request.query.search;
+    var googleResult = await rp(`http://www.google.com/search?q=${search}`);
+    var firstResult = getFirstResult(googleResult);
+    console.log(firstResult);
+    var content = await rp(`http://www.google.com/${firstResult}`);
+    response.end(content);
+});
+
+function getFirstResult(body){
+    var dom = new JSDOM(body);
+    var firstResult = dom.window.document.querySelectorAll("#ires a")[0].href;
+    return firstResult;
+}
+
+app.listen(3000, function(){
+    console.log(`Server is running...`);
+});
